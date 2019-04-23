9a2d1c3a542a2f8d9491f2308e59bd75eaac8bb6
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-04-22 23:37:43 -0700

    #4 Reading CSV and importing stuff.

---
+++ implementation2/q1.py
@@ -0,0 +1,10 @@
+import sys
+import csv
+import math
+
+def getRows(CSVPath):
+    with open(CSVPath, newline='') as csvFile:
+        rowReader = list(csv.reader(csvFile, delimiter=',', quotechar='|'))
+
+        return rowReader
+
