114f4d40710bd06e41ecac2c7414882cdc67e484
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-04-22 23:38:19 -0700

    #4 Created a quick and dirty prediction function.

--- implementation2/q1.py
+++ implementation2/q1.py
@@ -8,3 +8,28 @@ def getRows(CSVPath):
 
         return rowReader
 
+def predictPoint(k, featureList, exampleCancer):
+    sortDistanceList = []
+    benignVotes = 0
+    malignantVotes = 0
+
+    for cancerCell in featureList:
+        distance = 0.
+        for i in range(1,31):
+            distance += float(cancerCell[i]) - float(exampleCancer[i])
+        distance = abs(distance)
+        distance = math.sqrt(distance)
+        sortDistanceList.append((distance, cancerCell[0]))
+        sortDistanceList.sort()
+
+    for vote in range(k):
+        if(sortDistanceList[vote][1] == 1):
+            malignantVotes += 1
+        elif(sortDistanceList[vote][1] == -1):
+            benignVotes +=1
+
+    if(benignVotes > malignantVotes):
+        return -1
+    elif(malignantVotes > benignVotes):
+        return 1
+
