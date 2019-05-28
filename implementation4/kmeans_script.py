f2c318ffa61ae0efa460304ae1df3316fb87481a
Burton Jaursch <burton.jaursch@gmail.com>
2019-05-28 16:08:26 -0700

    adding steps for kmeans script in order to plot points. BETA

--- implementation4/kmeans_script.py
+++ implementation4/kmeans_script.py
@@ -1,6 +1,33 @@
 import os
+import numpy as np
 
+def plotSSE(sse, plotLabel, xLabel):
+    iterationList =  [(x + 1) for x in range(len(sse))]
+    plt.plot(iterationList, sse, label="SSE vs Iteration") #find how to count indexes of sse of x-axis
+    plt.ylabel("Sum of Squared Errors")
+    plt.xlabel("Iteration")
+    plt.legend()
+    plt.ylim(0, 1)
+    plt.show()
+
+#p2_1:
+cmd = "python kmeans.py 2"
+#collect sse values
+#sseK2 =
+#plotSSE(sseK2, "SSE vs Iteration", "Iteration")
+
+
+#p2_2
+#minSSEOfK = []
 for kClusters in [2, 4, 6, 8, 10]:
-    cmd = "python kmeans.py %d" % (kClusters)
+    #sseCurrentIteration = []
+    for x in range(10):
+        cmd = "python kmeans.py %d" % (kClusters)
+        #collect sse values of this iteration
+        #sseCurrentIteration.append()
+    #take lowest sse within sseCurrentIteration
+    #minSSEOfK.append(np.min(sseCurrentIteration))
 
-#don't run quite yet. May not be necessary. Need to explore assignment deliverables
+#plotSSE(minSSEOfK, "SSE vs K-value", "K-Value")
+
+###don't run quite yet. May not be necessary. Need to explore assignment deliverables
